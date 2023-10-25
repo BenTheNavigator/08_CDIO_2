22a3edfad1c9a99a67785601bfc032e48e051b1d
BenTheNavigator <benjaminlgad@gmail.com>
2023-10-25 12:49:27 +0200

    added player class

---
+++ CDIO2.java
@@ -0,0 +1,20 @@
+class Game {
+
+
+}
+
+class Player {
+    public String name;
+    public int ID;
+    public int accountBalance;
+
+    public Player (String name) {
+        this.name = name;
+        this.ID = ID++;
+        this.accountBalance = 1000;
+    }
+}
+
+class Account {
+    
+}
