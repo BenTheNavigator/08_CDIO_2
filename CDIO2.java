5a08cb80fadd20a1afb6bf2915989f3ce6037a28
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2023-10-26 14:10:28 +0200

    added actual balance

--- CDIO2.java
+++ CDIO2.java
@@ -25,46 +25,46 @@ class Game{
                 switch (rollsum){
                     case 2:
                         Player1.getAccount().addAmount(250);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 3:
                         Player1.getAccount().decreaseAmount(100);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 4:
                         Player1.getAccount().addAmount(100);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 5:
                         Player1.getAccount().decreaseAmount(20);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 6:
                         Player1.getAccount().addAmount(180);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 7:
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 8:
                         Player1.getAccount().decreaseAmount(70);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 9:
                         Player1.getAccount().addAmount(60);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 10:
                         Player1.getAccount().decreaseAmount(80);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 11:
                         Player1.getAccount().decreaseAmount(50);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
                     case 12:
                         Player1.getAccount().addAmount(650);
-                        Player1.getAccount().getBalance();
+                        System.out.println("New balance: " + Player1.getAccount().getBalance());
                         break;
 
                     
@@ -84,46 +84,46 @@ class Game{
                 switch (rollsum){
                     case 2:
                         Player2.getAccount().addAmount(250);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 3:
                         Player2.getAccount().decreaseAmount(100);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 4:
                         Player2.getAccount().addAmount(100);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 5:
                         Player2.getAccount().decreaseAmount(20);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 6:
                         Player2.getAccount().addAmount(180);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 7:
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 8:
                         Player2.getAccount().decreaseAmount(70);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 9:
                         Player2.getAccount().addAmount(60);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 10:
                         Player2.getAccount().decreaseAmount(80);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 11:
                         Player2.getAccount().decreaseAmount(50);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                     case 12:
                         Player2.getAccount().addAmount(650);
-                        Player2.getAccount().getBalance();
+                        System.out.println("New balance: " + Player2.getAccount().getBalance());
                         break;
                 }
                 if (Player2.getAccount().getBalance()>=3000){
